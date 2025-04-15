13e636b18bdff80bcc3c8f8058fc786ec058127f
Akase <akase097@gmail.com>
2025-04-15 15:05:03 +0900

    add Pch

---
+++ Source/Pch.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <Windows.h>
+#include <assert.h>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <array>
+#include <vector>
+#include <wrl.h>
+
+using Microsoft::WRL::ComPtr;
+
+
+// DirectX12
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <DirectXMath.h>
+using namespace DirectX;
+
+// DX12ShaderCompile
+#include <d3dcompiler.h>
+#pragma comment(lib, "d3dcompiler.lib")
+
+
