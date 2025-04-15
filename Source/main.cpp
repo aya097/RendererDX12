1bb4e4ea80496ea55fa6f382cc1a3c7fd6d02910
Akase <akase097@gmail.com>
2025-04-15 12:22:59 +0900

    add window in main

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,5 +2,14 @@
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+	constexpr int width = 1280;
+	constexpr int height = 720;
+	Window window(width, height, L"AAA", L"WINDOW");
 
+	while (!window.IsQuit())
+	{
+
+	}
+
+	return 0;
 }
