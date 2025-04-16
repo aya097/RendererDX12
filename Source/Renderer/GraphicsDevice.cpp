5c3fd51223c9d9f5c16cfdf747bd4132b4124794
Akase <akase097@gmail.com>
2025-04-16 16:51:14 +0900

    add Create Command Objects

--- Source/Renderer/GraphicsDevice.cpp
+++ Source/Renderer/GraphicsDevice.cpp
@@ -4,8 +4,13 @@ GraphicsDevice* g_GraphicsDevice;
 
 GraphicsDevice::GraphicsDevice(HWND hwnd, int width, int height)
 {
+#ifdef DEBUG
+	EnableDebugLayer();
+#endif // DEBUG
+
 	CreateDevice();
 	CreateFactory();
+	CreateCommandObjects();
 }
 
 
@@ -38,3 +43,43 @@ void GraphicsDevice::CreateFactory()
 		assert(false && "CreateFactory is failed");
 	}
 }
+
+void GraphicsDevice::CreateCommandObjects()
+{
+	auto result = _device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&_commandAllocator));
+
+	if (FAILED(result))
+	{
+		assert(false && "CreateCommandAlocatpr is failed");
+	}
+
+	result = _device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, _commandAllocator.Get(), nullptr, IID_PPV_ARGS(&_commandList));
+
+	if (FAILED(result))
+	{
+		assert(false && "CreateCommandList is failed");
+	}
+
+	D3D12_COMMAND_QUEUE_DESC commandQueueDesc = {};
+	commandQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	commandQueueDesc.NodeMask = 0;
+	commandQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+	commandQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+
+	result = _device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&_commandQueue));
+
+	if (FAILED(result))
+	{
+		assert(false && "CreateCommandQueue is failed");
+	}
+}
+
+void GraphicsDevice::EnableDebugLayer()
+{
+	ID3D12Debug* debugLayer = nullptr;
+	D3D12GetDebugInterface(IID_PPV_ARGS(&debugLayer));
+	debugLayer->EnableDebugLayer();
+	debugLayer->Release();
+}
+
+
--- Source/Renderer/GraphicsDevice.h
+++ Source/Renderer/GraphicsDevice.h
@@ -8,15 +8,19 @@ public:
 private:
 	void CreateDevice();
 	void CreateFactory();
-	//void CreateCommandList();
+	void CreateCommandObjects();
 	//void CreateSwapChain(HWND hwnd, int width, int height);
 	//void CreateFence();
 	//void SetBarrierTransition();
 	//void WaitRenderer();
-	//void EnableDebugLayer();
+	void EnableDebugLayer();
 
 	ComPtr<ID3D12Device9> _device = nullptr;
 	ComPtr<IDXGIFactory7> _dxgiFactory = nullptr;
+
+	ComPtr<ID3D12CommandAllocator> _commandAllocator = nullptr;
+	ComPtr<ID3D12GraphicsCommandList7> _commandList = nullptr;
+	ComPtr<ID3D12CommandQueue> _commandQueue = nullptr;
 };
 
 extern GraphicsDevice* g_GraphicsDevice;
