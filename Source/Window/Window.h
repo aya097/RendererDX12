2302ce5e3234b4c96084f222fda5b8994fe3dc09
Akase <akase097@gmail.com>
2025-04-16 15:38:53 +0900

    add Get width and height in window class

--- Source/Window/Window.h
+++ Source/Window/Window.h
@@ -5,7 +5,11 @@ class Window
 public:
 	Window(int clientWidth, int clientHeight, const std::wstring& titleName, const std::wstring& windowClassName);
 	const HWND GetHWND() const { return _hwnd; }
+	constexpr int GetWidth() const { return _width; }
+	constexpr int GetHeight() const { return _height; }
 	const bool IsQuit();
 private:
 	HWND _hwnd;
+	int _width;
+	int _height;
 };
